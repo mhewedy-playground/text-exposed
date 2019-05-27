0e401f52a424d0ef5bea20767d74f28da4362896
Muhammad Hewedy <mhewedy@gmail.com>
2019-05-28 00:57:54 +0300

    init

--- src/main/kotlin/DSL.kt
+++ src/main/kotlin/DSL.kt
@@ -1,37 +1,43 @@
 import org.jetbrains.exposed.dao.IntIdTable
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
+import kotlin.concurrent.thread
 
 fun main() {
 
-    Database.connect("jdbc:h2:mem:test;DATABASE_TO_UPPER=false", driver = "org.h2.Driver")
+    // to be able to monitor the app via jvisualvm
+    thread {
 
-    transaction {
-        addLogger(StdOutSqlLogger)
+        Database.connect("jdbc:h2:mem:test;DATABASE_TO_UPPER=false", driver = "org.h2.Driver")
 
-        SchemaUtils.create(Cities)
+        transaction {
+            addLogger(StdOutSqlLogger)
 
-        val cities = arrayListOf(
-            City("Cairo", 20_000_000),
-            City("Madrid", 20_000),
-            City("Riyadh", 99_933),
-            City("Hidrbad", 20_000_000),
-            City("Newyork", 20_000),
-            City("Bangladish", 20_000),
-            City("Kazablanka", 10_000_000)
-        )
-        Cities.batchInsert(cities) {
-            this[Cities.name] = it.name
-            this[Cities.population] = it.population
+            SchemaUtils.create(Cities)
+
+            val cities = arrayListOf(
+                City("Cairo", 20_000_000),
+                City("Madrid", 20_000),
+                City("Riyadh", 99_933),
+                City("Hidrbad", 20_000_000),
+                City("Newyork", 20_000),
+                City("Bangladish", 20_000),
+                City("Kazablanka", 10_000_000)
+            )
+
+
+            Cities.batchInsert(cities) {
+                this[Cities.name] = it.name
+                this[Cities.population] = it.population
+            }
+
+            Cities.slice(Cities.id, Cities.name, Cities.population)
+                .selectAll()
+                .andWhere { Cities.population greaterEq 10 }
+                .andWhere { Cities.name like "Cai%" }
+                .orderBy(Cities.id)
+                .forEach { println(it) }
         }
-
-        val citiesFromDb = Cities.slice(Cities.id, Cities.name, Cities.population)
-            .selectAll()
-            .andWhere { Cities.population greaterEq 10_000_000 }
-            .andWhere { Cities.name like "Cai%" }
-            .orderBy(Cities.id)
-            .map { City(it[Cities.name], it[Cities.population], it[Cities.id].value) }
-        println(citiesFromDb)
     }
 
 }
